6db4ccb53bc6e5835efd85ef859342b85b3b4b49
Ichwannur <ichwannur617@gmail.com>
2024-02-01 20:35:05 +0800

    login

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -14,19 +14,19 @@ class PostController extends Controller
     public function index()
     {
 
-        $title='';
-        if(request('category')){
-            $category = Category::firstWhere('slug',request('category'));
-            $title = ' in '. $category->name;
+        $title = '';
+        if (request('category')) {
+            $category = Category::firstWhere('slug', request('category'));
+            $title = ' in ' . $category->name;
         }
-        if(request('author')){
-            $author = User::firstWhere('username',request('author'));
-            $title = ' in '. $author->name;
+        if (request('author')) {
+            $author = User::firstWhere('username', request('author'));
+            $title = ' in ' . $author->name;
         }
         return view('posts', [
             "title" => "All Posts" . $title,
             "active" => "posts",
-            "posts" => Post::latest()->filter(request(['search','category','author']))->paginate(7)->withQueryString()
+            "posts" => Post::latest()->filter(request(['search', 'category', 'author']))->paginate(7)->withQueryString()
 
         ]);
     }

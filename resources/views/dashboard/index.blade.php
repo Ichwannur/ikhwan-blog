a5df62f8520735593de4597b9871dccaf440f84a
lukluu <laode.lukmana88@gmail.com>
2024-02-03 08:11:29 +0700

    d

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -237,6 +237,12 @@
                 </button>
               </form>
             </li>
+            <li class="nav-item">
+              <a class="nav-link d-flex align-items-center gap-2" href="/">
+                <svg class="bi"><use xlink:href="#file-earmark"/></svg>
+                Main
+              </a>
+            </li>
           </ul>
         </div>
       </div>
